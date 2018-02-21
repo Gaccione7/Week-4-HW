aa386752abf96f3558ea5d61d74a62aebae7ea43
Gaccione7 <35547797+Gaccione7@users.noreply.github.com>
2018-02-21 18:16:29 -0500

    Committed HW

---
+++ Part 1/movies-and-jurassic-park.js
@@ -0,0 +1,96 @@
+console.log('movies-and-jurassic-park.js is connected! woohoo!');
+
+
+// Part 1
+
+var matrix = {
+  title: 'Matrix',
+  director: 'The Wachowskis',
+  actors: ['Keanu Reeves', 'Laurence Fishburne', 'Huge Weaving'],
+  releaseYear: 1999,
+  duration: 146,
+  }
+
+funtion print() {
+  document.write ("Matrix");
+  print();
+}
+funtion print() {
+  document.write ("The Wachowskis");
+  print();
+}
+
+funtion print() { 
+  document.write ("1999");
+  print();
+
+var snakewaterMontana = {
+  paleontologist: "Dr. Alan Grant",
+  depth: "10 meters",
+  specimen: "Velociraptor"
+};
+
+
+// Do the tasks down here!
+
+
+var guestOfHonor = "Dr. Alan Grant";
+var cleverGirl = "Velociraptor"
+document.write (specimen);
+
+
+
+
+// Part 2
+
+var nicaragua = {
+  depth: "200 meters",
+  annual_budget: 1500000,
+  specimens: [
+    "Tyrannosaurus Rex",
+    "Stegosaurous",
+    "Triceratops",
+    "Velociraptor"
+  ],
+};
+
+// Do the tasks down here!
+
+var nicaraguanSpecimens = ['Tyrannosaurus Rex','Stegosaurous','Triceratops','Velociraptor'];
+document.write(nicaraguanSpecimens[3]);
+
+var budget = 1500000;
+var increase = 250000;
+var total = budget + increase
+document.write(total);
+
+
+// Part 3
+
+var hammondsMines = {
+  buenos_aires: {
+    depth: "400 meters",
+    annual_budget: 1000000,
+    specimens: [
+      "Dilophosaurus",
+      "Brachiosaurus"
+    ],
+  },
+  mexico: {
+    depth: "350 meters",
+    annual_budget: 900000,
+    specimens: [
+      "Gallimimus",
+      "Parasaurolophus"
+    ],
+  },
+};
+
+// Do the tasks down here!
+
+var depth = 350
+var budget = 1000000
+var specimen = ['Dilophosaurus','Brachiosaurus'];
+
+
+
---
+++ Part 2/main.js
@@ -0,0 +1,18 @@
+console.log('main.js is connected! woohoo!');
+
+
+var temp = new object {
+	temp.celsius = 34;
+	temp.farenheit = 45;
+
+temp.describe = function(){
+	return 'The temperature is '+this.celsius';
+	}
+
+
+
+
+function teperature (){
+	
+}
+}
